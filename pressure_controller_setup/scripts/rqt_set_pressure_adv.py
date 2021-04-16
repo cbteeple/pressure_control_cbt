199084ac77f17d89b212a326243b13bdbd851631
Clark Teeple <cbteeple@g.harvard.edu>
2021-04-16 17:58:22 -0400

    added advanced pressure control with configurable sliders

--- pressure_controller_setup/scripts/rqt_set_pressure_adv.py
+++ pressure_controller_setup/scripts/rqt_set_pressure_adv.py
@@ -2,9 +2,9 @@
 
 import sys
 
-from pressure_controller_setup.set_pressures_basic import MyPlugin
+from pressure_controller_setup.set_pressures_adv import MyPlugin
 from rqt_gui.main import Main
 
-plugin = 'pressure_controller_setup.set_pressures_basic'
+plugin = 'pressure_controller_setup.set_pressures_adv'
 main = Main(filename=plugin)
 sys.exit(main.main(standalone=plugin))
